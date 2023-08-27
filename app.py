05f08cf1540d193b1851df314e449de8d56362ce
loveeklund <loveekl@kth.se>
2023-08-27 07:48:36 +0200

    added cookies

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, make_response
 import base64
 import io
 import numpy as np
 from model.model import get_model
 from PIL import Image
 import pickle
-
 import random
 
 # Load dictionary using pickle
@@ -18,10 +17,11 @@ MODEL.load_weights("model/model.h5")
 
 app = Flask(__name__)
 
-LABEL_INDEX = random.randint(0, len(LABEL_MAPPING)-1)
-LABEL = LABEL_MAPPING[LABEL_INDEX]
+BACKUP_LABEL_INDEX = 0 
 
 def string_overlap(target, strings):
+    print(target)
+    print(strings)
     result = ''
 
     for i in range(len(target)):
@@ -57,31 +57,46 @@ def save_image():
     image = Image.open(io.BytesIO(image_data))
     image.save('input_image.png', 'PNG', quality=95)
 
+    cookie_value = request.cookies.get('label_index')
+    if cookie_value:
+        label_index = int(cookie_value)
+    else:
+        label_index = BACKUP_LABEL_INDEX
+    label = LABEL_MAPPING[label_index]
     predictions = score_image(image)
-
+    print(label)
+    print(label_index)
     for label in LABEL_MAPPING:
         print(f"p({LABEL_MAPPING[label]}) = {predictions[label]}")
-    #score = predictions[GUESS_INDEX]* 100
 
     top_guess = predictions.argmax()
     
     if max(predictions) >= 0.5:
-        if top_guess != LABEL_INDEX:
+        if top_guess != label_index:
             message = f"That's not it fam, looks like {LABEL_MAPPING[top_guess]} to me, I'll update the hint for you though"
-            out_hint = " ".join(string_overlap(LABEL,[LABEL_MAPPING[top_guess],in_hint]))
+            out_hint = " ".join(string_overlap(label,[LABEL_MAPPING[top_guess],in_hint]))
         else:
-            message = f"yes you got it, congrats! \nit was {LABEL_MAPPING[LABEL_INDEX]} i wanted"
-            out_hint = " ".join(LABEL)
+            message = f"yes you got it, congrats! \nit was {LABEL_MAPPING[label_index]} i wanted"
+            out_hint = " ".join(label)
     else:
         message = "what is that?"
         out_hint = " ".join(in_hint)
 
     return jsonify({"message": message, "newHint": out_hint})
 @app.route('/', methods=['GET'])
+
 def serve_page():
-    label = LABEL_MAPPING[LABEL_INDEX] 
+    label_index = random.randint(0,len(LABEL_MAPPING) - 1)
+    label = LABEL_MAPPING[label_index] 
     
-    return render_template("index.html", hint = "_ " * len(label))
+    rendered = render_template("index.html", hint = "_ " * len(label))
+
+    # Create a response object from the rendered string
+    resp = make_response(rendered)
+
+    # Set the cookie on the response object
+    resp.set_cookie('label_index', str(label_index))
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
